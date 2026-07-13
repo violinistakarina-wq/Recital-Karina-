d0f6665a92fc600e27e0d04bbd51dab4d3609462
violinistakarina-wq <violinista.karina@gmail.com>
2026-07-13 19:20:35 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sax-rotina-v1';
+const CACHE_NAME = 'sax-rotina-v2';
 const FILES_TO_CACHE = [
   './rotina-saxofone.html',
   './manifest.json',
@@ -25,6 +25,12 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
